fe372d7e7b699342f8da9c783b5841ab5c3bd8bc
n8fr8 <nathan@freitas.net>
2012-11-08 03:24:35 +0530

    added CertDisplayActivity to manifest
    updated sample app UI

--- sample/src/sample/onionkit/OnionKitSampleActivity.java
+++ sample/src/sample/onionkit/OnionKitSampleActivity.java
@@ -1,6 +1,7 @@
 package sample.onionkit;
 
 import info.guardianproject.onionkit.proxy.SocksHttpClient;
+import info.guardianproject.onionkit.trust.StrongHttpsClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -28,6 +29,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
@@ -45,6 +47,10 @@ public class OnionKitSampleActivity extends Activity {
 	private TextView txtView = null;
 	private EditText txtUrl = null;
 	
+	private String proxyHost = "localhost"; //test the local device proxy provided by Orbot/Tor
+	private int proxyHttpPort = 8118; //default for Orbot/Tor
+	private int proxySocksPort = 9050; //default for Orbot/Tor
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,62 +59,15 @@ public class OnionKitSampleActivity extends Activity {
         
         txtUrl = (EditText)findViewById(R.id.txtUrl);
         txtView = (TextView)findViewById(R.id.WizardTextBody);
-        Button btn = ((Button)findViewById(R.id.btnWizard1));
         
-        btn.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-			 
-				Runnable runnable = new Runnable ()
-				{
-					public void run()
-					{
-						//this opens a direct socks socket on port 80
-						URL url;
-						try {
-							url = new URL(txtUrl.getText().toString());
-
-							openSocksSocket(url.getHost(),80);
-						} catch (MalformedURLException e) {
-							Log.e(TAG,"bad url",e);
-        					txtView.setText("bad urL: " + e.getLocalizedMessage());
-						}
-						
-					}
-				};
-				
-				Handler handle = new Handler();
-				handle.post(runnable);
-			}
-        });
-
+        Button btn;
+        
         btn = ((Button)findViewById(R.id.btnWizard2));
         
         btn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 			 
-				Runnable runnable = new Runnable ()
-				{
-					
-					public void run ()
-					{
-						String url = txtUrl.getText().toString();
-						
-						try
-						{
-							String resp = checkHTTP(url, Proxy.Type.HTTP, "localhost", 8118);
-							txtView.setText("connection response: " + resp);
-						}
-						catch (Exception e)
-						{
-							Log.e(TAG,"error connecting to: " + url,e);
-							txtView.setText(e.getLocalizedMessage());
-						}
-					}
-				};
-				
-
-				Handler handle = new Handler();
-				handle.post(runnable);
+				new Thread(runnableNetHttp).start();
 			}
         });
         
@@ -118,93 +77,12 @@ public class OnionKitSampleActivity extends Activity {
         btn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 			 
-				Runnable runnable = new Runnable ()
-				{
-					
-					public void run ()
-					{
-						String url = txtUrl.getText().toString();
-						
-						try
-						{
-							String resp = checkHTTP(url, Proxy.Type.SOCKS, "localhost", 9050);
-							txtView.setText("connection response: " + resp);
-						}
-						catch (Exception e)
-						{
-							Log.e(TAG,"error connecting to: " + url,e);
-							txtView.setText(e.getLocalizedMessage());
-						}
-					}
-				};
-				
 
-				Handler handle = new Handler();
-				handle.post(runnable);
+				new Thread(runnableNetSocks).start();
 			}
         });
         
     }
-    
-    
-    public void openSocksSocket (String checkHost, int checkPort)
-    {
-    	txtView.setText("Opening socket to " + checkHost + " on port " + checkPort + "\n");
-    	
-    	try
-    	{
-    		
-    		Socks5Proxy proxy = new Socks5Proxy("localhost",9050);
-    		proxy.resolveAddrLocally(false);
-   
-    		SocksSocket ss = null;
-    		
-    		PrintWriter out = null;
-    		BufferedReader in = null;
-    		
-    		 try {
-    	       
-    			 ss = new SocksSocket(proxy,checkHost, checkPort);
-    			 
-    	            out = new PrintWriter(ss.getOutputStream(), true);
-    	            in = new BufferedReader(new InputStreamReader(
-    	                                        ss.getInputStream()));
-    	           
-    	    		
-    	            out.println("GET / HTTP/1.0");
-    	            out.println("Host: check.torproject.org");
-    	            out.println();
-    	            
-    	            String line = null;
-    	            
-    	            while ((line = in.readLine())!=null)
-    	            {
-    	            	txtView.append(line);
-    	            	txtView.append("\n");
-    	            }
-    	            
-    	            
-    	        } catch (UnknownHostException e) {
-    	            Log.i(TAG,"Could not find host",e);
-    	        } catch (IOException e) {
-    	            Log.i(TAG,"Error reading and writing",e);
-    	           
-    	        }
-
-    		
-
-    		out.close();
-    		in.close();
-    		
-    	}
-    	catch (Exception e)
-    	{
-    		txtView.append("error opening socket: " + e.toString());
-    		
-    		Log.e(TAG, "Unable to connect to torproject",e);
-    	}
-    }
-    
     
     public String checkHTTP (String url, Proxy.Type proxyType, String host, int port) throws KeyManagementException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, CertificateException, IOException
     {
@@ -218,8 +96,7 @@ public class OnionKitSampleActivity extends Activity {
     		}
     		else if (proxyType == Proxy.Type.HTTP)
     		{
-    		
-    			httpclient = new DefaultHttpClient();
+    			httpclient = new StrongHttpsClient(this);
         		HttpHost proxy = new HttpHost(host, port);
         		httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
     		}
@@ -230,4 +107,68 @@ public class OnionKitSampleActivity extends Activity {
     		return response.getStatusLine().getStatusCode() + "";
     	
     }
+    
+    Runnable runnableNetHttp = new Runnable ()
+	{
+		
+		public void run ()
+		{
+			String url = txtUrl.getText().toString();
+			
+			try
+			{
+				String resp = checkHTTP(url, Proxy.Type.HTTP, proxyHost, proxyHttpPort);
+				Message msg = new Message();
+				msg.getData().putString("status", resp);
+				handler.sendMessage(msg);
+
+			}
+			catch (Exception e)
+			{
+				String err = "error connecting to: " + url + "=" + e.toString();
+				Log.e(TAG,err,e);
+				Message msg = new Message();
+				msg.getData().putString("status", err);
+				handler.sendMessage(msg);
+			}
+		}
+	};
+	
+	 Runnable runnableNetSocks = new Runnable ()
+		{
+			
+			public void run ()
+			{
+				String url = txtUrl.getText().toString();
+				
+				try
+				{
+					String resp = checkHTTP(url, Proxy.Type.SOCKS, proxyHost, proxySocksPort);
+					Message msg = new Message();
+					msg.getData().putString("status", resp);
+					handler.sendMessage(msg);
+				}
+				catch (Exception e)
+				{
+					String err = "error connecting to: " + url + "=" + e.toString();
+					Log.e(TAG,err,e);
+					Message msg = new Message();
+					msg.getData().putString("status", err);
+					handler.sendMessage(msg);
+				}
+			}
+		};
+		
+		Handler handler = new Handler ()
+		{
+
+			@Override
+			public void handleMessage(Message msg) {
+				
+				String msgText = msg.getData().getString("status");
+				
+				txtView.setText(msgText);
+			}
+			
+		};
 }
