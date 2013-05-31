ac040986f23cb999fcc8d1128941157743d15d6a
Abel Luck <abel@guardianproject.info>
2013-05-31 17:08:26 +0200

    format library code

--- libonionkit/src/info/guardianproject/onionkit/proxy/SocksProxyClientConnOperator.java
+++ libonionkit/src/info/guardianproject/onionkit/proxy/SocksProxyClientConnOperator.java
@@ -1,3 +1,4 @@
+
 package info.guardianproject.onionkit.proxy;
 
 import info.guardianproject.onionkit.trust.StrongSSLSocketFactory;
@@ -22,19 +23,21 @@ import ch.boye.httpclientandroidlib.protocol.HttpContext;
 
 public class SocksProxyClientConnOperator extends DefaultClientConnectionOperator {
 
-	private String mProxyHost;
-	private int mProxyPort;
-	
-    public SocksProxyClientConnOperator(final SchemeRegistry schemes, String proxyHost, int proxyPort) {
+    private String mProxyHost;
+    private int mProxyPort;
+
+    public SocksProxyClientConnOperator(final SchemeRegistry schemes, String proxyHost,
+            int proxyPort) {
         super(schemes);
-        
+
         mProxyHost = proxyHost;
         mProxyPort = proxyPort;
     }
 
     @Override
     public void openConnection(final OperatedClientConnection conn, final HttpHost target,
-            final InetAddress local, final HttpContext context, final HttpParams params) throws IOException {
+            final InetAddress local, final HttpContext context, final HttpParams params)
+            throws IOException {
         if (conn == null) {
             throw new IllegalArgumentException("Connection may not be null");
         }
@@ -48,10 +51,9 @@ public class SocksProxyClientConnOperator extends DefaultClientConnectionOperato
             throw new IllegalStateException("Connection must not be open");
         }
 
-
         Scheme schm = schemeRegistry.getScheme(target.getSchemeName());
         SocketFactory sf = schm.getSocketFactory();
-      
+
         InetAddress[] addresses = InetAddress.getAllByName(target.getHostName());
 
         int port = schm.resolvePort(target.getPort());
@@ -59,31 +61,33 @@ public class SocksProxyClientConnOperator extends DefaultClientConnectionOperato
         InetSocketAddress socksAddr = new InetSocketAddress(mProxyHost, mProxyPort);
         Proxy proxy = new Proxy(Proxy.Type.SOCKS, socksAddr);
         Socket sock = new Socket(proxy);
-        
+
         if (sf instanceof StrongSSLSocketFactory)
         {
-        	sock.connect( new InetSocketAddress(target.getHostName(),port));
-        	sock = ((StrongSSLSocketFactory)sf).createSocket(sock, target.getHostName(), port, true);
+            sock.connect(new InetSocketAddress(target.getHostName(), port));
+            sock = ((StrongSSLSocketFactory) sf).createSocket(sock, target.getHostName(), port,
+                    true);
         }
-        
+
         conn.opening(sock, target);
 
         for (int i = 0; i < addresses.length; i++) {
             InetAddress address = addresses[i];
             boolean last = i == addresses.length - 1;
-            
+
             try {
-              
-            	if (!sock.isConnected())
-            	{
-	            	Socket connsock = sf.connectSocket(sock, address.getHostAddress(), port, local, 0, params);
-	            	
-	                if (sock != connsock) {
-	                    sock = connsock;
-	                    conn.opening(sock, target);
-	                }
-            	}
-            	
+
+                if (!sock.isConnected())
+                {
+                    Socket connsock = sf.connectSocket(sock, address.getHostAddress(), port, local,
+                            0, params);
+
+                    if (sock != connsock) {
+                        sock = connsock;
+                        conn.opening(sock, target);
+                    }
+                }
+
                 prepareSocket(sock, context, params);
                 conn.openCompleted(sf.isSecure(sock), params);
                 break;
