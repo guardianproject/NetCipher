174fabd9a59458087307ba3f11102d1a206d2bf0
n8fr8 <nathan@freitas.net>
2014-02-13 14:05:11 -0500

    fixed SOCKS DNS leak issue #2958

--- libonionkit/src/info/guardianproject/onionkit/proxy/SocksProxyClientConnOperator.java
+++ libonionkit/src/info/guardianproject/onionkit/proxy/SocksProxyClientConnOperator.java
@@ -1,105 +1,133 @@
 
 package info.guardianproject.onionkit.proxy;
 
-import info.guardianproject.onionkit.trust.StrongSSLSocketFactory;
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import ch.boye.httpclientandroidlib.HttpHost;
-import ch.boye.httpclientandroidlib.conn.ConnectTimeoutException;
-import ch.boye.httpclientandroidlib.conn.HttpHostConnectException;
 import ch.boye.httpclientandroidlib.conn.OperatedClientConnection;
 import ch.boye.httpclientandroidlib.conn.scheme.Scheme;
 import ch.boye.httpclientandroidlib.conn.scheme.SchemeRegistry;
-import ch.boye.httpclientandroidlib.conn.scheme.SocketFactory;
+import ch.boye.httpclientandroidlib.conn.ssl.SSLSocketFactory;
 import ch.boye.httpclientandroidlib.impl.conn.DefaultClientConnectionOperator;
 import ch.boye.httpclientandroidlib.params.HttpParams;
 import ch.boye.httpclientandroidlib.protocol.HttpContext;
 
 public class SocksProxyClientConnOperator extends DefaultClientConnectionOperator {
 
-    private String mProxyHost;
-    private int mProxyPort;
+	private static final int CONNECT_TIMEOUT_MILLISECONDS = 60000;
+	private static final int READ_TIMEOUT_MILLISECONDS = 60000;
 
-    public SocksProxyClientConnOperator(final SchemeRegistry schemes, String proxyHost,
-            int proxyPort) {
-        super(schemes);
-
+	private String mProxyHost;
+	private int mProxyPort;
+	    
+	public SocksProxyClientConnOperator(SchemeRegistry registry, String proxyHost, int proxyPort) {
+        super(registry);
+        
         mProxyHost = proxyHost;
         mProxyPort = proxyPort;
     }
 
+    // Derived from the original DefaultClientConnectionOperator.java in Apache HttpClient 4.2
     @Override
-    public void openConnection(final OperatedClientConnection conn, final HttpHost target,
-            final InetAddress local, final HttpContext context, final HttpParams params)
-            throws IOException {
-        if (conn == null) {
-            throw new IllegalArgumentException("Connection may not be null");
-        }
-        if (target == null) {
-            throw new IllegalArgumentException("Target host may not be null");
-        }
-        if (params == null) {
-            throw new IllegalArgumentException("Parameters may not be null");
-        }
-        if (conn.isOpen()) {
-            throw new IllegalStateException("Connection must not be open");
-        }
-
-        Scheme schm = schemeRegistry.getScheme(target.getSchemeName());
-        SocketFactory sf = schm.getSocketFactory();
-
-        InetAddress[] addresses = InetAddress.getAllByName(target.getHostName());
-
-        int port = schm.resolvePort(target.getPort());
-
-        InetSocketAddress socksAddr = new InetSocketAddress(mProxyHost, mProxyPort);
-        Proxy proxy = new Proxy(Proxy.Type.SOCKS, socksAddr);
-        Socket sock = new Socket(proxy);
-
-        if (sf instanceof StrongSSLSocketFactory)
-        {
-            sock.connect(new InetSocketAddress(target.getHostName(), port));
-            sock = ((StrongSSLSocketFactory) sf).createSocket(sock, target.getHostName(), port,
-                    true);
-        }
-
-        conn.opening(sock, target);
-
-        for (int i = 0; i < addresses.length; i++) {
-            InetAddress address = addresses[i];
-            boolean last = i == addresses.length - 1;
-
-            try {
-
-                if (!sock.isConnected())
-                {
-                    Socket connsock = sf.connectSocket(sock, address.getHostAddress(), port, local,
-                            0, params);
-
-                    if (sock != connsock) {
-                        sock = connsock;
-                        conn.opening(sock, target);
-                    }
-                }
-
-                prepareSocket(sock, context, params);
-                conn.openCompleted(sf.isSecure(sock), params);
-                break;
-            } catch (ConnectException ex) {
-                if (last) {
-                    throw new HttpHostConnectException(target, ex);
-                }
-            } catch (ConnectTimeoutException ex) {
-                if (last) {
-                    throw ex;
-                }
+    public void openConnection(
+            final OperatedClientConnection conn,
+            final HttpHost target,
+            final InetAddress local,
+            final HttpContext context,
+            final HttpParams params) throws IOException {
+        Socket socket = null;
+        Socket sslSocket = null;
+        try {
+            if (conn == null || target == null || params == null) {
+                throw new IllegalArgumentException("Required argument may not be null");
             }
+            if (conn.isOpen()) {
+                throw new IllegalStateException("Connection must not be open");
+            }
+
+            Scheme scheme = schemeRegistry.getScheme(target.getSchemeName());
+            SSLSocketFactory sslSocketFactory = (SSLSocketFactory)scheme.getSchemeSocketFactory();
+
+            int port = scheme.resolvePort(target.getPort());
+            String host = target.getHostName();
+
+            // Perform explicit SOCKS4a connection request. SOCKS4a supports remote host name resolution
+            // (i.e., Tor resolves the hostname, which may be an onion address).
+            // The Android (Apache Harmony) Socket class appears to support only SOCKS4 and throws an
+            // exception on an address created using INetAddress.createUnresolved() -- so the typical
+            // technique for using Java SOCKS4a/5 doesn't appear to work on Android:
+            // https://android.googlesource.com/platform/libcore/+/master/luni/src/main/java/java/net/PlainSocketImpl.java
+            // See also: http://www.mit.edu/~foley/TinFoil/src/tinfoil/TorLib.java, for a similar implementation
+
+            // From http://en.wikipedia.org/wiki/SOCKS#SOCKS4a:
+            //
+            // field 1: SOCKS version number, 1 byte, must be 0x04 for this version
+            // field 2: command code, 1 byte:
+            //     0x01 = establish a TCP/IP stream connection
+            //     0x02 = establish a TCP/IP port binding
+            // field 3: network byte order port number, 2 bytes
+            // field 4: deliberate invalid IP address, 4 bytes, first three must be 0x00 and the last one must not be 0x00
+            // field 5: the user ID string, variable length, terminated with a null (0x00)
+            // field 6: the domain name of the host we want to contact, variable length, terminated with a null (0x00)
+
+
+            socket = new Socket();
+            conn.opening(socket, target);
+            socket.setSoTimeout(READ_TIMEOUT_MILLISECONDS);
+            socket.connect(new InetSocketAddress(mProxyHost, mProxyPort), CONNECT_TIMEOUT_MILLISECONDS);
+
+            DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+            outputStream.write((byte)0x04);
+            outputStream.write((byte)0x01);
+            outputStream.writeShort((short)port);
+            outputStream.writeInt(0x01);
+            outputStream.write((byte)0x00);
+            outputStream.write(host.getBytes());
+            outputStream.write((byte)0x00);
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+            if (inputStream.readByte() != (byte)0x00 || inputStream.readByte() != (byte)0x5a) {
+                throw new IOException("SOCKS4a connect failed");
+            }
+            inputStream.readShort();
+            inputStream.readInt();
+
+            sslSocket = sslSocketFactory.createLayeredSocket(socket, host, port, params);
+            conn.opening(sslSocket, target);
+            sslSocket.setSoTimeout(READ_TIMEOUT_MILLISECONDS);
+            prepareSocket(sslSocket, context, params);
+            conn.openCompleted(sslSocketFactory.isSecure(sslSocket), params);
+            // TODO: clarify which connection throws java.net.SocketTimeoutException?
+        } catch (IOException e) {
+            try {
+                if (sslSocket != null) {
+                    sslSocket.close();
+                }
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException ioe) {}
+            throw e;
         }
+    }
+
+    @Override
+    public void updateSecureConnection(
+            final OperatedClientConnection conn,
+            final HttpHost target,
+            final HttpContext context,
+            final HttpParams params) throws IOException {
+        throw new RuntimeException("operation not supported");
+    }
+
+    @Override
+    protected InetAddress[] resolveHostname(final String host) throws UnknownHostException {
+        throw new RuntimeException("operation not supported");
     }
 }
