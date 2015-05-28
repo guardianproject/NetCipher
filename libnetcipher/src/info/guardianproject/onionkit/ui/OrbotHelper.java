65e10815059fa8cdf4f4edaaed3a1448a1403a5b
Hans-Christoph Steiner <hans@eds.org>
2015-05-28 19:05:03 -0400

    modernize download Intent and choose between FDroid, Play, and f-droid.org
    This fixes up the Intent used to take the user to install Orbot.  First,
    it looks for Google Play or FDroid, and if neither are installed, then it
    takes the user to the f-droid.org page for Orbot.  Google Play and F-Droid
    are our only supported download methods for Orbot.  Taking users to other
    markets, like MiMarket, Baidu, Amazon, etc. will just give them a confusing
    blank result.
    
    closes #408 https://dev.guardianproject.info/issues/408

--- libnetcipher/src/info/guardianproject/onionkit/ui/OrbotHelper.java
+++ libnetcipher/src/info/guardianproject/onionkit/ui/OrbotHelper.java
@@ -3,14 +3,18 @@ package info.guardianproject.onionkit.ui;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
 
 import info.guardianproject.onionkit.R;
+
+import java.util.List;
 
 public class OrbotHelper {
 
@@ -18,14 +22,19 @@ public class OrbotHelper {
 
     public final static String ORBOT_PACKAGE_NAME = "org.torproject.android";
     public final static String ORBOT_MARKET_URI = "market://details?id=" + ORBOT_PACKAGE_NAME;
-    public final static String ORBOT_FDROID_URI = "https://f-droid.org/repository/browse/?fdid=org" + ORBOT_PACKAGE_NAME;
-    public final static String ORBOT_PLAY_URI = "https://play.google.com/store/apps/details?id=" + ORBOT_PACKAGE_NAME;
+    public final static String ORBOT_FDROID_URI = "https://f-droid.org/repository/browse/?fdid="
+            + ORBOT_PACKAGE_NAME;
+    public final static String ORBOT_PLAY_URI = "https://play.google.com/store/apps/details?id="
+            + ORBOT_PACKAGE_NAME;
 
     public final static String TOR_BIN_PATH = "/data/data/" + ORBOT_PACKAGE_NAME + "/app_bin/tor";
 
     public final static String ACTION_START_TOR = "org.torproject.android.START_TOR";
     public final static String ACTION_REQUEST_HS = "org.torproject.android.REQUEST_HS_PORT";
     public final static int HS_REQUEST_CODE = 9999;
+
+    private final static String FDROID_PACKAGE_NAME = "org.fdroid.fdroid";
+    private final static String PLAY_PACKAGE_NAME = "com.android.vending";
 
     private Context mContext = null;
 
@@ -58,39 +67,53 @@ public class OrbotHelper {
         return installed;
     }
 
-    public void promptToInstall(Activity activity)
+    public void promptToInstall(final Activity activity)
     {
-        // show dialog - install from market, f-droid or direct APK
-        showDownloadDialog(activity, activity.getString(R.string.install_orbot_),
-                activity.getString(R.string.you_must_have_orbot),
-                activity.getString(R.string.yes), activity.getString(R.string.no));
-    }
+        /*
+         * Check if installing from F-Droid or Google Play, otherwise take the
+         * user to the Orbot download page on f-droid.org.
+         */
+        String message = activity.getString(R.string.you_must_have_orbot) + "  ";
+        activity.getString(R.string.get_orbot_from_google_play);
 
-    private static AlertDialog showDownloadDialog(final Activity activity,
-            CharSequence stringTitle, CharSequence stringMessage, CharSequence stringButtonYes,
-            CharSequence stringButtonNo) {
-        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(activity);
-        downloadDialog.setTitle(stringTitle);
-        downloadDialog.setMessage(stringMessage);
-        downloadDialog.setPositiveButton(stringButtonYes, new DialogInterface.OnClickListener() {
+        PackageManager pm = activity.getPackageManager();
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(ORBOT_MARKET_URI));
+        List<ResolveInfo> resInfos = pm.queryIntentActivities(intent, 0);
+        String foundPackageName = null;
+        for (ResolveInfo r : resInfos) {
+            Log.i("OrbotHelper", "market: " + r.activityInfo.packageName);
+            if (TextUtils.equals(r.activityInfo.packageName, FDROID_PACKAGE_NAME)
+                    || TextUtils.equals(r.activityInfo.packageName, PLAY_PACKAGE_NAME)) {
+                foundPackageName = r.activityInfo.packageName;
+                break;
+            }
+        }
+
+        if (foundPackageName == null) {
+            intent.setData(Uri.parse(ORBOT_FDROID_URI));
+            message += activity.getString(R.string.get_orbot_from_fdroid);
+        } else {
+            intent.setPackage(foundPackageName);
+            message += activity.getString(R.string.download_orbot_from_fdroid);
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle(R.string.install_orbot_);
+        builder.setMessage(message);
+        builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                try {
-                    intent.setData(Uri.parse(ORBOT_MARKET_URI));
-                    activity.startActivity(intent);
-                } catch (ActivityNotFoundException e) {
-                    intent.setData(Uri.parse(ORBOT_FDROID_URI));
-                    activity.startActivity(intent);
-                }
+                Log.i("orbothelper", "package ");
+                activity.startActivity(intent);
             }
         });
-        downloadDialog.setNegativeButton(stringButtonNo, new DialogInterface.OnClickListener() {
+        builder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
             }
         });
-        return downloadDialog.show();
+        builder.show();
     }
 
     public void requestOrbotStart(final Activity activity)
