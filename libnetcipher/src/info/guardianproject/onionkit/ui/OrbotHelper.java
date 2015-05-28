16818a7b65caf0ac6bdd1e74ba42e3527d032992
Hans-Christoph Steiner <hans@eds.org>
2015-05-28 17:33:04 -0400

    use correct market: URL, and fall back to FDroid HTTP URL if no market app
    The standard way to link to an app is the market: URL, the pname: thing
    that Orbot has been using does not seem to be supported anymore, so this
    updates to the well known, supported market: URL.  If there is no handler
    for the market: scheme installed, it will instead prompt the user to get it
    via direct download from FDroid.
    
    https://developer.android.com/distribute/tools/promote/linking.html
    https://stackoverflow.com/questions/11753000/how-to-open-the-google-play-store-directly-from-my-android-application

--- libnetcipher/src/info/guardianproject/onionkit/ui/OrbotHelper.java
+++ libnetcipher/src/info/guardianproject/onionkit/ui/OrbotHelper.java
@@ -3,6 +3,7 @@ package info.guardianproject.onionkit.ui;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -16,7 +17,11 @@ public class OrbotHelper {
     private final static int REQUEST_CODE_STATUS = 100;
 
     public final static String ORBOT_PACKAGE_NAME = "org.torproject.android";
-    public final static String TOR_BIN_PATH = "/data/data/org.torproject.android/app_bin/tor";
+    public final static String ORBOT_MARKET_URI = "market://details?id=" + ORBOT_PACKAGE_NAME;
+    public final static String ORBOT_FDROID_URI = "https://f-droid.org/repository/browse/?fdid=org" + ORBOT_PACKAGE_NAME;
+    public final static String ORBOT_PLAY_URI = "https://play.google.com/store/apps/details?id=" + ORBOT_PACKAGE_NAME;
+
+    public final static String TOR_BIN_PATH = "/data/data/" + ORBOT_PACKAGE_NAME + "/app_bin/tor";
 
     public final static String ACTION_START_TOR = "org.torproject.android.START_TOR";
     public final static String ACTION_REQUEST_HS = "org.torproject.android.REQUEST_HS_PORT";
@@ -55,25 +60,29 @@ public class OrbotHelper {
 
     public void promptToInstall(Activity activity)
     {
-        String uriMarket = activity.getString(R.string.market_orbot);
         // show dialog - install from market, f-droid or direct APK
         showDownloadDialog(activity, activity.getString(R.string.install_orbot_),
                 activity.getString(R.string.you_must_have_orbot),
-                activity.getString(R.string.yes), activity.getString(R.string.no), uriMarket);
+                activity.getString(R.string.yes), activity.getString(R.string.no));
     }
 
     private static AlertDialog showDownloadDialog(final Activity activity,
             CharSequence stringTitle, CharSequence stringMessage, CharSequence stringButtonYes,
-            CharSequence stringButtonNo, final String uriString) {
+            CharSequence stringButtonNo) {
         AlertDialog.Builder downloadDialog = new AlertDialog.Builder(activity);
         downloadDialog.setTitle(stringTitle);
         downloadDialog.setMessage(stringMessage);
         downloadDialog.setPositiveButton(stringButtonYes, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Uri uri = Uri.parse(uriString);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                activity.startActivity(intent);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                try {
+                    intent.setData(Uri.parse(ORBOT_MARKET_URI));
+                    activity.startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    intent.setData(Uri.parse(ORBOT_FDROID_URI));
+                    activity.startActivity(intent);
+                }
             }
         });
         downloadDialog.setNegativeButton(stringButtonNo, new DialogInterface.OnClickListener() {
