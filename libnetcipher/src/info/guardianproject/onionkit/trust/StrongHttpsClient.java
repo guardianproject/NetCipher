1ef21ade0e0a1dba83cc283caa8d050accdb4228
Hans-Christoph Steiner <hans@eds.org>
2015-05-29 15:33:10 -0400

    don't second guess the system on which X509TrustManager to use
    SSLContext will use the first instance of X509TrustManager that it finds,
    so this lib does not need to parse the TrustManagers for it.  Removing this
    means this lib touches less of the crazy, crufty, difficult certificate
    code for TLS.
    
    https://developer.android.com/reference/javax/net/ssl/SSLContext.html#init(javax.net.ssl.KeyManager[], javax.net.ssl.TrustManager[], java.security.SecureRandom)

--- libnetcipher/src/info/guardianproject/onionkit/trust/StrongHttpsClient.java
+++ libnetcipher/src/info/guardianproject/onionkit/trust/StrongHttpsClient.java
@@ -1,6 +1,17 @@
 
 package info.guardianproject.onionkit.trust;
 
+import android.content.Context;
+import android.util.Log;
+
+import ch.boye.httpclientandroidlib.HttpHost;
+import ch.boye.httpclientandroidlib.conn.ClientConnectionOperator;
+import ch.boye.httpclientandroidlib.conn.params.ConnRoutePNames;
+import ch.boye.httpclientandroidlib.conn.scheme.PlainSocketFactory;
+import ch.boye.httpclientandroidlib.conn.scheme.Scheme;
+import ch.boye.httpclientandroidlib.conn.scheme.SchemeRegistry;
+import ch.boye.httpclientandroidlib.impl.client.DefaultHttpClient;
+import ch.boye.httpclientandroidlib.impl.conn.tsccm.ThreadSafeClientConnManager;
 import info.guardianproject.onionkit.R;
 import info.guardianproject.onionkit.proxy.MyThreadSafeClientConnManager;
 import info.guardianproject.onionkit.proxy.SocksProxyClientConnOperator;
@@ -12,20 +23,7 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 
-import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
-
-import android.content.Context;
-import android.util.Log;
-import ch.boye.httpclientandroidlib.HttpHost;
-import ch.boye.httpclientandroidlib.conn.ClientConnectionOperator;
-import ch.boye.httpclientandroidlib.conn.params.ConnRoutePNames;
-import ch.boye.httpclientandroidlib.conn.scheme.PlainSocketFactory;
-import ch.boye.httpclientandroidlib.conn.scheme.Scheme;
-import ch.boye.httpclientandroidlib.conn.scheme.SchemeRegistry;
-import ch.boye.httpclientandroidlib.impl.client.DefaultHttpClient;
-import ch.boye.httpclientandroidlib.impl.conn.tsccm.ThreadSafeClientConnManager;
 
 public class StrongHttpsClient extends DefaultHttpClient {
 
@@ -34,7 +32,6 @@ public class StrongHttpsClient extends DefaultHttpClient {
     private String proxyType;
 
     private StrongSSLSocketFactory sFactory;
-    private TrustManager mTrustManager;
     private SchemeRegistry mRegistry;
 
     private final static String TRUSTSTORE_TYPE = "BKS";
@@ -49,17 +46,10 @@ public class StrongHttpsClient extends DefaultHttpClient {
 
 
         try {
-
             KeyStore keyStore = loadKeyStore();
             TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
             trustManagerFactory.init(keyStore);
-        	for (TrustManager trustManager : trustManagerFactory.getTrustManagers()) {          	  
-        	    if (trustManager instanceof X509TrustManager) {  
-        	    	mTrustManager = trustManagerFactory.getTrustManagers()[0];  
-        	    }  
-        	}  
-        	
-            sFactory = new StrongSSLSocketFactory(context, mTrustManager, keyStore, TRUSTSTORE_PASSWORD);
+            sFactory = new StrongSSLSocketFactory(context, trustManagerFactory.getTrustManagers(), keyStore, TRUSTSTORE_PASSWORD);
             mRegistry.register(new Scheme("https", 443, sFactory));
         } catch (Exception e) {
             throw new AssertionError(e);
@@ -76,7 +66,7 @@ public class StrongHttpsClient extends DefaultHttpClient {
 
         return trustStore;
     }
-    
+
     public StrongHttpsClient(Context context, KeyStore keystore) {
         this.context = context;
 
@@ -86,13 +76,7 @@ public class StrongHttpsClient extends DefaultHttpClient {
 
         try {
         	TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-        	for (TrustManager trustManager : trustManagerFactory.getTrustManagers()) {          	  
-        	    if (trustManager instanceof X509TrustManager) {  
-        	    	mTrustManager = trustManagerFactory.getTrustManagers()[0];  
-        	    }  
-        	} 
-
-            sFactory = new StrongSSLSocketFactory(context, mTrustManager, keystore, TRUSTSTORE_PASSWORD);
+            sFactory = new StrongSSLSocketFactory(context, trustManagerFactory.getTrustManagers(), keystore, TRUSTSTORE_PASSWORD);
             mRegistry.register(new Scheme("https", 443, sFactory));
         } catch (Exception e) {
             throw new AssertionError(e);
@@ -134,11 +118,6 @@ public class StrongHttpsClient extends DefaultHttpClient {
         }
     }
 
-    public TrustManager getTrustManager()
-    {
-        return mTrustManager;
-    }
-
     public void useProxy(boolean enableTor, String type, String host, int port)
     {
         if (enableTor)
@@ -152,7 +131,7 @@ public class StrongHttpsClient extends DefaultHttpClient {
             else
             {
             	proxyHost = new HttpHost(host, port, type);
-                getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxyHost);                
+                getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxyHost);
             }
         }
         else
