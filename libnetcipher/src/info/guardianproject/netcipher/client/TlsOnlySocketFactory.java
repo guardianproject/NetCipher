50b9e864fffb971763df91763f935b71abe4c9b9
Hans-Christoph Steiner <hans@eds.org>
2016-02-16 12:49:01 +0100

    add license blurb and copyright holders to all Java files

--- libnetcipher/src/info/guardianproject/netcipher/client/TlsOnlySocketFactory.java
+++ libnetcipher/src/info/guardianproject/netcipher/client/TlsOnlySocketFactory.java
@@ -1,5 +1,7 @@
 /*
  * Copyright 2015 Bhavit Singh Sengar
+ * Copyright 2015-2016 Hans-Christoph Steiner
+ * Copyright 2015-2016 Nathan Freitas
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
