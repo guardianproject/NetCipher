4877a0c8c4eced84d874717b51f1f5d4bea176a3
Mark Murphy <mmurphy@commonsware.com>
2016-05-07 18:00:50 -0400

    incorporated AOSP weak set code as replacement for API Level 9 impl

--- libnetcipher/src/info/guardianproject/netcipher/proxy/OrbotHelper.java
+++ libnetcipher/src/info/guardianproject/netcipher/proxy/OrbotHelper.java
@@ -36,6 +36,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.WeakHashMap;
 
@@ -316,9 +317,9 @@ public class OrbotHelper implements ProxyHelper {
     private boolean isInstalled=false;
     private Intent lastStatusIntent=null;
     private Set<StatusCallback> statusCallbacks=
-      Collections.newSetFromMap(new WeakHashMap<StatusCallback, Boolean>());
+      newSetFromMap(new WeakHashMap<StatusCallback, Boolean>());
     private Set<InstallCallback> installCallbacks=
-      Collections.newSetFromMap(new WeakHashMap<InstallCallback, Boolean>());
+      newSetFromMap(new WeakHashMap<InstallCallback, Boolean>());
     private long statusTimeoutMs=30000L;
     private long installTimeoutMs=60000L;
 
@@ -632,4 +633,27 @@ public class OrbotHelper implements ProxyHelper {
         }
     };
 
+    /*
+     *  Licensed to the Apache Software Foundation (ASF) under one or more
+     *  contributor license agreements.  See the NOTICE file distributed with
+     *  this work for additional information regarding copyright ownership.
+     *  The ASF licenses this file to You under the Apache License, Version 2.0
+     *  (the "License"); you may not use this file except in compliance with
+     *  the License.  You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     *  Unless required by applicable law or agreed to in writing, software
+     *  distributed under the License is distributed on an "AS IS" BASIS,
+     *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     *  See the License for the specific language governing permissions and
+     *  limitations under the License.
+     */
+
+    static <E> Set<E> newSetFromMap(Map<E, Boolean> map) {
+        if (map.isEmpty()) {
+            return new SetFromMap<E>(map);
+        }
+        throw new IllegalArgumentException("map not empty");
+    }
 }
